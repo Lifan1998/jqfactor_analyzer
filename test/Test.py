76772778ef31fe44827382dd538ff17e102ee3cc
lifan <2224779926@qq.com>
2023-04-23 18:18:52 +0800

    fix

--- test/Test.py
+++ test/Test.py
@@ -12,6 +12,20 @@ jqdatasdk.auth('15055735530', 'JoinQuant1998')
 from jqfactor_analyzer.sample import VOL5
 factor_data = VOL5
 
+
+#获取因子值pandas.DataFrame
+factor_data=jqdatasdk.get_factor_values(securities=['000300.XSHG'], factors=['MA5'],
+                  start_date='2018-01-01', end_date='2018-03-01')['MA5']
+
+#使用获取的因子值进行单因子分析
+far = ja.analyze_factor(factor=factor_data, start_date='2018-01-01', end_date='2018-03-01', weight_method='mktcap', industry='jq_l1', quantiles=8, periods=(1,5,22),max_loss=0.2)
+
+#分析结束后通过不同属性获取数据
+far.mean_return_std_by_quantile #获取按分位数分组加权平均因子收益
+
+
+far.create_full_tear_sheet(demeaned=False, group_adjust=False, by_group=False, turnover_periods=None, avgretplot=(5, 15), std_bar=False)
+
 # 对因子进行分析
 far = ja.analyze_factor(
     factor_data,  # factor_data 为因子值的 pandas.DataFrame
--- test/Test1.py
+++ test/Test1.py
@@ -10,20 +10,26 @@ jqdatasdk.auth('15055735530', 'JoinQuant1998')
 #     start_date='2022-04-28',
 #     end_date='2023-04-10')['VOL5']
 
-data = jqdatasdk.get_industries()
+# 申万一级行业
+data = jqdatasdk.get_industries(name='sw_l1')
+data = data.drop(columns=['start_date'])
+print(f'data:{data}')
+
+data = jqdatasdk.get_security_info2('801750')
 print(f'data:{data=}')
 
-data = jqdatasdk.get_security_info2('000300.XSHG')
-print(f'data:{data=}')
+
+# data = jqdatasdk.get_concepts()
+# print(f'data:{data=}')
 
 
-data = jqdatasdk.get_concepts()
-print(f'data:{data=}')
-
-
-data = jqdatasdk.get_price(security='000300.XSHG', start_date = '2023-04-10', end_date = '2023-04-20', frequency='daily')
+data = jqdatasdk.get_price(security='000300.XSHG', start_date = '2023-04-10', end_date = '2023-04-24', frequency='daily')
 print(f'data:\n{data}')
 
 data = jqdatasdk.get_bars(security='000300.XSHG', count=10, unit='1d', include_now=True, fields=("date", "open", "high", "low", "close",'volume','money'))
 print(f'data:\n{data}')
+
+#
+
+
 
